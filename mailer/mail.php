525d2f0aabd0e47c5d2b8674cabbee21c84311e6
telesco <telescolawrence@gmail.com>
2023-11-10 23:46:13 +0300

    Update mail.php

--- mailer/mail.php
+++ mailer/mail.php
@@ -2,10 +2,8 @@
 require_once 'mail_header.php';
 if($_GET['action'] == 'reset'){
     $password = $_GET['pd'];
-    $mail->Subject = "Recen Password Reset";
-    $mail->Body = "Your account password has been reset to : <b>{$password}</b>, kindly click the link below to activate your account and remember to change your password. <br>
-    <a href=\"www.refoveo.recen.io/include/activate.php?id={$to}\" class='button'>Activate</a>
-    ";
+    $mail->Subject = "Password Reset";
+    $mail->Body = "Your account password has been reset to : <b>{$password}</b>, kindly click the link below to activate your account and remember to change your password ";
     
     if(!$mail->Send()){
         echo 'Mailer Error: ' . $mail->ErrorInfo;
